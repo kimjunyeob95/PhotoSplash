ba20d4bdd9cd607bd59e5c91daec696913884c5e
junyeob kim <junyeob.kim@onch3.co.kr>
2024-09-09 15:23:59 +0900

    feat: nav ui 작업

---
+++ src/components/common/navigation/CommonNav.tsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import styles from './CommonNav.module.scss'
+import navJson from './nav.json'
+import { Link } from 'react-router-dom'
+
+interface Navigation {
+	index      : number
+	path       : string
+	label      : string
+	searchValue: string
+	isActive   : boolean
+}
+
+function CommonNav() {
+	const [navigation, setNavigation] = useState<Navigation[]>(navJson);
+
+	const navLinks = navigation.map((item: Navigation) => {
+		return (
+			<Link to={item.path} className={styles.navigation__menu} key={item.path} >
+				<span className={styles.navigation__menu__label}>{item.label}</span>
+			</Link>
+		)
+	});
+
+	return (
+		<div className={styles.navigation}>{navLinks}</div>
+	)
+}
+
+export default CommonNav
